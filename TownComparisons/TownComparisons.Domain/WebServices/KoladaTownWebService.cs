338ae4a5024d68ad812b861940b614ac390ad9ac
Ola Franzén <of222au@student.lnu.se>
2016-01-26 17:43:43 +0100

    Project base files

---
+++ TownComparisons/TownComparisons.Domain/WebServices/KoladaTownWebService.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TownComparisons.Domain.Abstract;
+using TownComparisons.Domain.Entities;
+
+namespace TownComparisons.Domain.WebServices
+{
+    public class KoladaTownWebService : TownWebServiceBase
+    {
+        //(kod för att anropa Kolada API här i olika funktioner)
+        
+
+        public override Operator GetTownOperatorData(Operator operator_)
+        {
+
+
+            throw new NotImplementedException();
+        }
+
+        public override List<Operator> GetTownOperators(Municipality municipiality, Category category)
+        {
+
+
+            throw new NotImplementedException();
+        }
+
+    }
+}
