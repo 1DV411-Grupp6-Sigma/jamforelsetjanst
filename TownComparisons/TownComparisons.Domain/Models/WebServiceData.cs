74710476e9404793240654753ccb4ee4602798e7
Ola Franzén <of222au@student.lnu.se>
2016-02-09 12:04:31 +0100

    feat(Domain): Prepared new Models classes to be used as more generic classes in Service class etc

---
+++ TownComparisons/TownComparisons.Domain/Models/WebServiceData.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TownComparisons.Domain.Models
+{
+    public class WebServiceData
+    {
+        public WebServiceQuery Query { get; set; }
+
+        public OrganisationalUnit Ou { get; set; }
+
+        public int Period { get; set; }
+
+        public List<WebServiceDataValue> Values { get; set; }
+
+    }
+}
