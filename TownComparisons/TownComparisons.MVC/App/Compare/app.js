98afbe5d80fd2b54b126c0ac8ca439bb2bb405b5
Andreas Bom <andreas.bom@gmail.com>
2016-02-15 14:31:35 +0100

    refactor(Domain::Service) Cache functionallity implemented

--- TownComparisons/TownComparisons.MVC/App/Compare/app.js
+++ TownComparisons/TownComparisons.MVC/App/Compare/app.js
@@ -1,4 +1,24 @@
 ﻿//Compare module
-(function () {
-    //
-})();
+
+    angular.module('compare', [])
+        .controller('compareViewModel', function() {
+            var vm = this;
+            //List that contains selected OU
+            vm.selectedOuList = [];
+            vm.hello = "Hello from compareViewModel";
+            vm.addToCompareList = function(organisationalUnit) {
+                selectedOuList.push(organisationalUnit);
+            };
+        });
+
+
+
+//Module for testing
+
+    mainModule.controller('andreasCategoryViewModel', function(compare) {
+        var vm = this;
+        vm.hello = "Hello!";
+        vm.compare.addToCompareList("ID");
+        vm.list = compare.selectedOuList;
+    });
+
---
+++ TownComparisons/TownComparisons.MVC/Controllers/AndreasTestController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace TownComparisons.MVC.Controllers
+{
+    public class AndreasTestController : Controller
+    {
+        // GET: AndreasTest
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
