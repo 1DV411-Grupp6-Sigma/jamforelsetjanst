7a94654098de17936b6cc971199f368027be249c
Andreas Bom <andreas.bom@gmail.com>
2016-02-23 19:45:32 +0100

    refactor(Andular::Compare) saves compare list to cookie

--- TownComparisons/TownComparisons.MVC/App_Start/BundleConfig.cs
+++ TownComparisons/TownComparisons.MVC/App_Start/BundleConfig.cs
@@ -8,6 +8,8 @@ namespace TownComparisons.MVC
         // For more information on bundling, visit http://go.microsoft.com/fwlink/?LinkId=301862
         public static void RegisterBundles(BundleCollection bundles)
         {
+            BundleTable.EnableOptimizations = false;
+
             bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
                         "~/Scripts/jquery-{version}.js"));
 
@@ -17,7 +19,8 @@ namespace TownComparisons.MVC
             //Add angular as bundle
             bundles.Add(new ScriptBundle("~/bundles/angular").Include(
                         "~/Scripts/angular.js",
-                        "~/Scripts/angular-route.js"));
+                        "~/Scripts/angular-route.js", 
+                        "~/Scripts/angular-cookies.js"));
 
             // Use the development version of Modernizr to develop with and learn from. Then, when you're
             // ready for production, use the build tool at http://modernizr.com to pick only the tests you need.
