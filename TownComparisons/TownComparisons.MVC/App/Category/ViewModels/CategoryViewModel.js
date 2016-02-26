dc9f058bf044061df09ead02b0596a1d49cbf0cb
Julia Sivartsson <jsigc09@student.lnu.se>
2016-02-26 12:34:04 +0100

    design: Added foundation icons and Jämför-buttons

--- TownComparisons/TownComparisons.MVC/App/Category/ViewModels/CategoryViewModel.js
+++ TownComparisons/TownComparisons.MVC/App/Category/ViewModels/CategoryViewModel.js
@@ -8,6 +8,22 @@
         $scope.getOrganisationalUntsByCategoryId($routeParams.categoryId);
     }
 
+    $scope.getStandardSettings = function (ou) {
+        ou.class = "before-compare";
+        ou.icon = "fi-plus";
+        ou.text = "Jämför";
+    }
+
+    $scope.checkWhichSchoolsAreCompared = function (subject) {
+        for (var i = 0; i < collectorFactory.listOfSubjects.length; i++) {
+            if (collectorFactory.listOfSubjects[i].OrganisationalUnitId === subject.OrganisationalUnitId) {
+                    subject.class = "after-compare";
+                    subject.icon = "fi-check";
+                    subject.text = "";
+            }
+        }
+    }
+
     $scope.getOrganisationalUntsByCategoryId = function (categoryId) {
         viewModelHelper.apiGet('api/operators_in_category/' + categoryId, null,
             function (result) {
