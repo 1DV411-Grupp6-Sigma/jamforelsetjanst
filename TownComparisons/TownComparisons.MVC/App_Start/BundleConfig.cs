a688eb99917c89aff9801b53e3e83489c0149cc9
Andreas Bom <andreas.bom@gmail.com>
2016-03-08 09:19:21 +0100

    angular-aria.js added. accessability added to Nav and Layout. Removed som dead code

--- TownComparisons/TownComparisons.MVC/App_Start/BundleConfig.cs
+++ TownComparisons/TownComparisons.MVC/App_Start/BundleConfig.cs
@@ -20,7 +20,8 @@ namespace TownComparisons.MVC
             bundles.Add(new ScriptBundle("~/bundles/angular").Include(
                         "~/Scripts/angular.js",
                         "~/Scripts/angular-route.js", 
-                        "~/Scripts/angular-cookies.js"));
+                        "~/Scripts/angular-cookies.js",
+                        "~/Scripts/angular-aria.js"));
 
             // Use the development version of Modernizr to develop with and learn from. Then, when you're
             // ready for production, use the build tool at http://modernizr.com to pick only the tests you need.
